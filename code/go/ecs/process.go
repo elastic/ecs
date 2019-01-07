daec4d5234f1e5b7fb956aabcade731f050a67b6
Andrew Kroh <andrew.kroh@elastic.co>
2019-01-07 10:19:54 -0500

    Generate process.args as []string in Go code (#296)
    Add special handling for the process.args field to ensure that it's generated as a []string.

--- code/go/ecs/process.go
+++ code/go/ecs/process.go
@@ -40,7 +40,7 @@ type Process struct {
 
 	// Array of process arguments.
 	// May be filtered to protect sensitive information.
-	Args string `ecs:"args"`
+	Args []string `ecs:"args"`
 
 	// Absolute path to the process executable.
 	Executable string `ecs:"executable"`
