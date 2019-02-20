73c4ad11fbd71cb58c564ebea0a00e520e4b0af6
Mathieu Martin <mathieu.martin@elastic.co>
2019-02-20 16:12:29 -0500

    Still very much a WIP, but:
    - Flat field list generated in generated/ecs/fields_flat.yml
    - Nested field list (similar to structure of schemas/*.yml) in generated/ecs/fields_nested.yml
    - Reusable fields are correctly listed in fields_flat.yml only, at this time.
    - Added a separate test file that's for sanity checks. More about the state of the ECS spec than testing code corner cases.

--- scripts/schema_reader.py
+++ scripts/schema_reader.py
@@ -3,10 +3,6 @@ import yaml
 
 import pprint
 
-# class SchemaReader:
-#     def __init__(self):
-#         files = find_files("schemas/*.yml")
-
 ## File loading stuff
 
 def schema_files():
@@ -48,10 +44,13 @@ def dict_clean_string_values(dict):
 
 ## Finalize in memory representation of ECS field definitions (e.g. defaults, cleanup)
 
+### Schema level (data about the field sets)
+
 def schema_cleanup_values(schema):
     dict_clean_string_values(schema)
     schema_set_default_values(schema)
     schema_set_fieldset_prefix(schema)
+    schema_fields_as_dictionary(schema)
 
 def schema_set_default_values(schema):
     schema['type'] = 'group'
@@ -64,6 +63,14 @@ def schema_set_fieldset_prefix(schema):
     else:
         schema['prefix'] = schema['name'] + '.'
 
+def schema_fields_as_dictionary(schema):
+    """Re-nest the array of field names as a dictionary of 'fieldname' => { field definition }"""
+    field_array = schema.pop('fields')
+    schema['fields'] = {}
+    for field in field_array:
+        schema['fields'][field['name']] = field
+
+### Field definitions
 
 def field_cleanup_values(field, prefix):
     dict_clean_string_values(field)
@@ -75,7 +82,7 @@ def field_set_flat_name(field, prefix):
 
 def field_set_defaults(field):
     if 'index' in field and field['index'] == False:
-        field.delete('type')
+        field.pop('key', None)
     if 'short' not in field:
         field['short'] = field['description']
     if 'multi_fields' in field:
@@ -88,26 +95,48 @@ def field_set_multi_field_defaults(parent_field):
             mf['name'] = mf['type']
         mf['flat_name'] = parent_field['flat_name'] + '.' + mf['name']
 
-        # Not sure if we'll need:
-        # if 'description' not in multi_fields:
-        #     multi_fields['description'] = field['description']
-        # if 'example' not in multi_fields:
-        #     multi_fields['example'] = field['example']
+def duplicate_reusable_fieldsets(schema, fields_flat):
+    """Copies reusable field definitions to their expected places in the flattened schema only"""
+    if 'reusable' in schema:
+        for new_nesting in schema['reusable']['expected']:
 
-def finalize_schemas(schemas):
-    for schema_name in schemas:
-        schema = schemas[schema_name]
-        schema_set_default_values(schema)
+            for (name, field) in schema['fields'].items():
+                # Poor folks deepcopy, sorry -- A Rubyist
+                copied_field = field.copy()
+                if 'multi_fields' in copied_field:
+                    copied_field['multi_fields'] = copied_field['multi_fields'].copy()
 
-        for field in schema['fields']:
+                destination_name = new_nesting + '.' + field['flat_name']
+                copied_field['flat_name'] = destination_name
+                copied_field['original_fieldset'] = schema['name']
+                fields_flat[destination_name] = copied_field
+
+## Main
+
+def finalize_schemas(fields_nested, fields_flat):
+    for schema_name in fields_nested:
+        schema = fields_nested[schema_name]
+        schema_cleanup_values(schema)
+
+        for (name, field) in schema['fields'].items():
             field_cleanup_values(field, schema['prefix'])
 
-        # interpret_markdown(schema)
+            fields_flat[field['flat_name']] = field
+
+        # TODO duplicate in nested too?
+        duplicate_reusable_fieldsets(schema, fields_flat)
+
+def load_ecs():
+    fields_nested = load_schema_files()
+    fields_flat = {}
+    finalize_schemas(fields_nested, fields_flat)
+    return (fields_nested, fields_flat)
 
 if __name__ == '__main__':
-    fields_nested = load_schema_files()
-    # fields_nested = load_schema_files(['schemas/base.yml'])
+    (ecs_nested, ecs_fields) = load_ecs()
 
-    finalize_schemas(fields_nested)
-    # print pprint.pprint(fields_nested)
-    print pprint.pprint(fields_nested['event'])
+    print sorted(ecs_fields.keys())
+    with open('generated/ecs/fields_flat.yml', 'w') as outfile:
+        yaml.dump(ecs_fields, outfile, default_flow_style=False)
+    with open('generated/ecs/fields_nested.yml', 'w') as outfile:
+        yaml.dump(ecs_nested, outfile, default_flow_style=False)
---
+++ scripts/tests/test_ecs_spec.py
@@ -0,0 +1,26 @@
+import sys
+import os
+
+import unittest
+
+current_dir = os.path.dirname(__file__)
+sys.path.append(os.path.join(current_dir, '..'))
+
+import schema_reader
+
+class TestEcsSpec(unittest.TestCase):
+    """Sanity check for things that should be true in the ECS spec."""
+
+    def setUp(self):
+        (nested, flat) = schema_reader.load_ecs()
+        self.ecs_nested = nested
+        self.ecs_fields = flat
+
+    def test_base_flat_name(self):
+        self.assertIsInstance(self.ecs_fields['@timestamp'], dict)
+        self.assertEqual(
+                self.ecs_nested['base']['fields']['@timestamp']['flat_name'],
+                '@timestamp')
+
+if __name__ == '__main__':
+    unittest.main()
