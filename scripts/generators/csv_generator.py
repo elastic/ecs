fb1715a35b4670f4fefe4373e1a7242c6a60e29a
Mathieu Martin <mathieu.martin@elastic.co>
2019-02-21 15:52:14 -0500

    fields.csv now lists base fields first

--- scripts/generators/csv_generator.py
+++ scripts/generators/csv_generator.py
@@ -1,10 +1,24 @@
 import csv
 import sys
 
-def generate(ecs_fields):
+
+def base_first(ecs_flat):
+    base_list = []
+    sorted_list = []
+    for field_name in sorted(ecs_flat):
+        if '.' in field_name:
+            sorted_list.append(ecs_flat[field_name])
+        else:
+            base_list.append(ecs_flat[field_name])
+    return base_list + sorted_list
+
+
+def generate(ecs_flat):
     open_mode = "wb"
     if sys.version_info >= (3, 0):
         open_mode = "w"
+
+    sorted_fields = base_first(ecs_flat)
 
     with open('generated/csv/fields.csv', open_mode) as csvfile:
         schema_writer = csv.writer(csvfile,
@@ -12,6 +26,5 @@ def generate(ecs_fields):
                                    quoting=csv.QUOTE_MINIMAL,
                                    lineterminator='\n')
         schema_writer.writerow(["Field", "Type", "Level", "Example"])
-        for field_name in sorted(ecs_fields):
-            field = ecs_fields[field_name]
-            schema_writer.writerow([field_name, field["type"], field["level"], field.get('example', '')])
+        for field in sorted_fields:
+            schema_writer.writerow([field['flat_name'], field['type'], field['level'], field.get('example', '')])
