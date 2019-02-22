4ddef9202a45f8c053861cce109baa2156d26f97
Mathieu Martin <mathieu.martin@elastic.co>
2019-02-22 15:13:58 -0500

    The generator for intermediary files isn't special. Now with other generators

--- scripts/generator.py
+++ scripts/generator.py
@@ -1,18 +1,22 @@
-import yaml
 import argparse
 
 import schema_reader
+from generators import intermediate_files
 from generators import csv_generator
 from generators import es_template
 
 
-def intermediate_files():
-    (ecs_nested, ecs_fields) = schema_reader.load_ecs()
-    with open('generated/ecs/fields_flat.yml', 'w') as outfile:
-        yaml.dump(ecs_fields, outfile, default_flow_style=False)
-    with open('generated/ecs/fields_nested.yml', 'w') as outfile:
-        yaml.dump(ecs_nested, outfile, default_flow_style=False)
-    return (ecs_nested, ecs_fields)
+def main():
+    args = argument_parser()
+
+    (ecs_nested, ecs_flat) = schema_reader.load_ecs()
+
+    intermediate_files.generate(ecs_nested, ecs_flat)
+    if args.intermediate_only:
+        exit
+
+    csv_generator.generate(ecs_flat)
+    es_template.generate(ecs_flat)
 
 
 def argument_parser():
@@ -24,12 +28,4 @@ def argument_parser():
 
 
 if __name__ == '__main__':
-    args = argument_parser()
-
-    (ecs_nested, ecs_flat) = intermediate_files()
-
-    if args.intermediate_only:
-        exit
-
-    csv_generator.generate(ecs_flat)
-    es_template.generate(ecs_flat)
+    main()
