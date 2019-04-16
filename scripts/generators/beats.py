c96cb10789b33f1dfbdd97d0adac9f7fbe543901
Mathieu Martin <webmat@gmail.com>
2019-04-16 10:17:59 -0400

    Backport #425 to 1.0: Specify static output format for event.duration (#432)
    Backport of PR #425 to 1.0 branch. Original message:
    
    Static ms output format isn't great, it's just the least bad option

--- scripts/generators/beats.py
+++ scripts/generators/beats.py
@@ -30,8 +30,8 @@ def generate(ecs_nested, ecs_version):
 
 def fieldset_field_array(source_fields):
     allowed_keys = ['name', 'level', 'required', 'type', 'object_type',
-                    'ignore_above', 'multi_fields', 'format',
-                    'description', 'example']
+                    'ignore_above', 'multi_fields', 'format', 'input_format',
+                    'output_format', 'output_precision', 'description', 'example']
     fields = []
     for nested_field_name in source_fields:
         ecs_field = source_fields[nested_field_name]
