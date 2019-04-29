180d75856e2ef4a74110beb790c2cc83feb5653e
Mathieu Martin <mathieu.martin@elastic.co>
2019-04-29 16:38:13 -0400

    Revert "Tweak file timestamp descriptions further"
    This reverts commit 1022e8240c3c32fb8e3bfaa9ba20e35a1ce07230.

--- code/go/ecs/file.go
+++ code/go/ecs/file.go
@@ -76,10 +76,10 @@ type File struct {
 	// File creation time.
 	Crtime time.Time `ecs:"crtime"`
 
-	// Last time the file content was modified.
+	// Last time file content was modified.
 	Mtime time.Time `ecs:"mtime"`
 
-	// Last time the file metadata was modified.
+	// Last time file metadata was modified.
 	Ctime time.Time `ecs:"ctime"`
 
 	// Last time the file was accessed.
