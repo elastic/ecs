64bd28ef3f276cc04fdf63ee47573dcc93f9982b
Derek Ditch <derek.ditch@elastic.co>
2019-11-13 14:51:11 -0600

    Reworked descriptions per PR discussion

--- code/go/ecs/tls.go
+++ code/go/ecs/tls.go
@@ -23,13 +23,15 @@ import (
 	"time"
 )
 
-// Fields related to TLS activity.
+// Fields related to a TLS connection. These fields focus on the TLS protocol
+// itself and intentionally avoids in-depth analysis of the related x.509
+// certificate files.
 type Tls struct {
-	// Normalized lowercase protocol name parsed from original string.
-	VersionProtocol string `ecs:"version.protocol"`
-
 	// Numeric part of the version parsed from the original string.
-	VersionNumber string `ecs:"version.number"`
+	Version string `ecs:"version"`
+
+	// Normalized lowercase protocol name parsed from original string.
+	VersionProtocol string `ecs:"version_protocol"`
 
 	// String indicating the cipher used during the current connection.
 	Cipher string `ecs:"cipher"`
@@ -41,7 +43,9 @@ type Tls struct {
 	// existing TLS negotiation.
 	Resumed bool `ecs:"resumed"`
 
-	// String indicating the protocol being tunneled.
+	// String indicating the protocol being tunneled. Per the values in the
+	// https://www.iana.org/assignments/tls-extensiontype-values/tls-extensiontype-values.xhtml#alpn-protocol-ids[IANA
+	// registry], this string should be lower case.
 	NextProtocol string `ecs:"next_protocol"`
 
 	// A hash that identifies clients based on how they perform an SSL/TLS
@@ -49,7 +53,8 @@ type Tls struct {
 	ClientJa3 string `ecs:"client.ja3"`
 
 	// Also called an SNI, this tells the server which hostname to which the
-	// client is attempting to connect.
+	// client is attempting to connect. When this value is available, it should
+	// get copied to `destination.domain`.
 	ClientServerName string `ecs:"client.server_name"`
 
 	// List of ciphers offered by the client during the client hello.
@@ -70,11 +75,15 @@ type Tls struct {
 	// valid.
 	ClientNotAfter time.Time `ecs:"client.not_after"`
 
-	// List of PEM-encoded certificates that make up the certificate chain
-	// offered by the client.
+	// Array of PEM-encoded certificates that make up the certificate chain
+	// offered by the client. This is usually mutually-exclusive of
+	// `client.certificate` since that value should be the first certificate in
+	// the chain.
 	ClientCertificateChain string `ecs:"client.certificate_chain"`
 
-	// PEM-encoded stand-alone certificate offered by the client.
+	// PEM-encoded stand-alone certificate offered by the client. This is
+	// usually mutually-exclusive of `client.certificate_chain` since this
+	// value also exists in that list.
 	ClientCertificate string `ecs:"client.certificate"`
 
 	// Certificate fingerprint using the MD5 digest of DER-encoded version of
@@ -112,11 +121,15 @@ type Tls struct {
 	// valid.
 	ServerNotAfter time.Time `ecs:"server.not_after"`
 
-	// List of PEM-encoded certificates that make up the certificate chain
-	// offered by the server.
+	// Array of PEM-encoded certificates that make up the certificate chain
+	// offered by the server. This is usually mutually-exclusive of
+	// `server.certificate` since that value should be the first certificate in
+	// the chain.
 	ServerCertificateChain string `ecs:"server.certificate_chain"`
 
-	// PEM-encoded stand-alone certificate offered by the server.
+	// PEM-encoded stand-alone certificate offered by the server. This is
+	// usually mutually-exclusive of `server.certificate_chain` since this
+	// value also exists in that list.
 	ServerCertificate string `ecs:"server.certificate"`
 
 	// Certificate fingerprint using the MD5 digest of DER-encoded version of
