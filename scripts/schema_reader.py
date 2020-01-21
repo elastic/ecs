87a15ef3ce3f726d53670ca771de1657a3c3114a
Marshall Main <marshall.main@elastic.co>
2020-01-21 10:52:44 -0500

    remove logic for '.' notation in reusable field names

--- scripts/schema_reader.py
+++ scripts/schema_reader.py
@@ -128,18 +128,12 @@ def duplicate_reusable_fieldsets(schema, fields_nested):
     # which is in turn reusable in a few places.
     if 'reusable' in schema:
         for new_nesting in schema['reusable']['expected']:
-            split_flat_name = new_nesting.split('.', 1)
-            top_level = split_flat_name[0]
             # List field set names expected under another field set.
             # E.g. host.nestings = [ 'geo', 'os', 'user' ]
-            nestings = fields_nested[top_level].setdefault('nestings', [])
-            if schema['name'] not in nestings:
-                nestings.append(schema['name'])
+            nestings = fields_nested[new_nesting].setdefault('nestings', [])
+            nestings.append(schema['name'])
             nestings.sort()
-            nested_schema = fields_nested
-            for level in split_flat_name:
-                nested_schema = nested_schema[level]['fields']
-            nested_schema[schema['name']] = schema
+            fields_nested[new_nesting]['fields'][schema['name']] = schema
 
 # Main
 
