b710b190289a08f7659513e350ac549eb2105fd0
Mathieu Martin <mathieu.martin@elastic.co>
2020-03-04 08:38:21 -0500

    Typo fixes

--- code/go/ecs/event.go
+++ code/go/ecs/event.go
@@ -187,11 +187,11 @@ type Event struct {
 	Ingested time.Time `ecs:"ingested"`
 
 	// Reference URL linking to additional information about this event.
-	// This URL links to a static definition of the this event Alert events,
+	// This URL links to a static definition of the this event. Alert events,
 	// indicated by `event.kind:alert`, are a common use case for this field.
 	Reference string `ecs:"reference"`
 
-	// URL linking to an external system to continue investigtion of this
+	// URL linking to an external system to continue investigation of this
 	// event.
 	// This URL links to another system where in-depth investigation of the
 	// specific occurence of this event can take place. Alert events, indicated
