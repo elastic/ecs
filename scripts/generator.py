49862bb834ca87cf3291ed8e3423e16571843eb6
Marshall Main <marshall.main@elastic.co>
2020-03-30 16:22:11 -0400

    dont make asciidocs with subset or include options

--- scripts/generator.py
+++ scripts/generator.py
@@ -57,6 +57,8 @@ def main():
 
     csv_generator.generate(flat, ecs_version, out_dir)
     es_template.generate(flat, ecs_version, out_dir)
+    if args.include or args.subset:
+        exit()
     beats.generate(nested, ecs_version, out_dir)
     asciidoc_fields.generate(intermediate_fields, ecs_version, docs_dir)
 
