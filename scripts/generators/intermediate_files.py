8e836ca577792383b4079bbeee71aac6375391dc
Mathieu Martin <mathieu.martin@elastic.co>
2020-06-01 14:16:13 -0400

    WIP on intermediate_files.py generator for ECS YAML files

--- scripts/generators/intermediate_files.py
+++ scripts/generators/intermediate_files.py
@@ -1,9 +1,13 @@
+import copy
+
+from schema import visitor
 from generators import ecs_helpers
 from os.path import join
 
 
 def generate(fields, out_dir):
-    nested, flat = generate_nested_flat(fields)
+    flat = generate_fully_flattened_fields(fields)
+    nested = {} #generate_partially_flattened_fields(fields)
 
     ecs_helpers.make_dirs(join(out_dir, 'ecs'))
     ecs_helpers.yaml_dump(join(out_dir, 'ecs/ecs_flat.yml'), flat)
@@ -12,49 +16,40 @@ def generate(fields, out_dir):
     return nested, flat
 
 
-def generate_nested_flat(fields_intermediate):
-    for field_name, field in fields_intermediate.items():
-        nestings = find_nestings(field['fields'], field_name + ".")
-        nestings.sort()
-        if len(nestings) > 0:
-            field['nestings'] = nestings
-    fields_nested = generate_partially_flattened_fields(fields_intermediate)
-    fields_flat = generate_fully_flattened_fields(fields_intermediate)
-    return (fields_nested, fields_flat)
-
-
-def find_nestings(fields, prefix):
-    """Recursively finds all reusable fields in the fields dictionary."""
-    nestings = []
-    for field_name, field in fields.items():
-        if 'reusable' in field:
-            nestings.append(prefix + field_name)
-        if 'fields' in field:
-            nestings.extend(find_nestings(field['fields'], prefix + field_name + '.'))
-    return nestings
-
-
-def generate_partially_flattened_fields(fields_nested):
-    flat_fields = {}
-    for (name, field) in fields_nested.items():
-        # assigning field.copy() adds all the top level schema fields, has to be a copy since we're about
-        # to reassign the 'fields' key and we don't want to modify fields_nested
-        flat_fields[name] = field.copy()
-        flat_fields[name]['fields'] = flatten_fields(field['fields'], "")
-    return flat_fields
-
-
-def generate_fully_flattened_fields(fields_nested):
-    flattened = flatten_fields(remove_non_root_reusables(fields_nested), "")
+def generate_fully_flattened_fields(fields):
+    filtered = remove_non_root_reusables(fields)
+    flattened = {}
+    visitor.visit_fields_with_memo(filtered, get_flat_fields, flattened)
     return flattened
+
+
+def get_flat_fields(details, memo):
+    '''Visitor function that accumulates all flat field details in the memo dict'''
+    if 'schema_details' in details or ecs_helpers.is_intermediate(details):
+        return
+    field_details = copy.deepcopy(details['field_details'])
+    if 'leaf_name' in field_details:
+        field_details.pop('leaf_name')
+    flat_name = field_details['flat_name']
+    memo[flat_name] = field_details
 
 
 def remove_non_root_reusables(fields_nested):
     fields = {}
     for (name, field) in fields_nested.items():
-        if 'reusable' not in field or ('reusable' in field and field['reusable']['top_level']):
+        if 'reusable' not in field['schema_details'] or field['schema_details']['reusable']['top_level']:
             fields[name] = field
     return fields
+
+
+def generate_partially_flattened_fields(fields):
+    flat_fields = {}
+    for (name, field) in fields.items():
+        # assigning field.copy() adds all the top level schema fields, has to be a copy since we're about
+        # to reassign the 'fields' key and we don't want to modify fields_nested
+        flat_fields[name] = field.copy()
+        flat_fields[name]['fields'] = flatten_fields(field['fields'], "")
+    return flat_fields
 
 
 def flatten_fields(fields, key_prefix):
@@ -65,7 +60,7 @@ def flatten_fields(fields, key_prefix):
             flat_fields[new_key] = field['field_details'].copy()
         if 'fields' in field:
             new_prefix = new_key + "."
-            if 'root' in field and field['root']:
+            if 'root' in field['schema_details'] and field['schema_details']['root']:
                 new_prefix = ""
             flat_fields.update(flatten_fields(field['fields'], new_prefix))
     return flat_fields
