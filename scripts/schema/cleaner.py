24e6111e213b165b817c8f426bcbe3d3153b8835
Mathieu Martin <mathieu.martin@elastic.co>
2020-06-17 15:19:50 -0400

    Raise when short descriptions are too long.

--- scripts/schema/cleaner.py
+++ scripts/schema/cleaner.py
@@ -174,9 +174,13 @@ def field_assertions_and_warnings(field):
 
 # Common
 
-
+SHORT_LIMIT = 120
 def single_line_short_description(schema_or_field):
-    if "\n" in schema_or_field['field_details']['short']:
-        msg = ("Short descriptions must be single line.\n" +
-               "Fieldset: '{}'\n{}".format(schema_or_field['field_details']['name'], schema_or_field))
+    short_length = len(schema_or_field['field_details']['short'])
+    if "\n" in schema_or_field['field_details']['short'] or short_length > SHORT_LIMIT:
+        msg = "Short descriptions must be single line, and under {} characters (current length: {}).\n".format(
+                   SHORT_LIMIT, short_length)
+        msg += "Offending field or field set: {}\nShort description:\n  {}".format(
+                   schema_or_field['field_details']['name'],
+                   schema_or_field['field_details']['short'])
         raise ValueError(msg)
