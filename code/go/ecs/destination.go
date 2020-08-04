34a2f46eca9bf802e9bdbed1f2c665b95f7540b0
Dain Perkins <dain.perkins@elastic.co>
2020-08-04 16:46:35 -0400

    Updated Domain fields
    removed country code
    added is_internationalized
    added further qualification for subdomain, tld
    replicated updates to client, server, source, destination, url, host, and dns.question

--- code/go/ecs/destination.go
+++ code/go/ecs/destination.go
@@ -51,8 +51,14 @@ type Destination struct {
 	// registered_domain, subdomain, and hostname as they are available.
 	Name string `ecs:"name"`
 
+	// Hostname of the system.
+	// It normally contains what the `hostname` command returns on the host
+	// machine, or the host portion of a fully qualified domain name.
+	Hostname string `ecs:"hostname"`
+
 	// The highest registered domain, stripped of the subdomain.
-	// For example, the registered domain for "foo.google.com" is "google.com".
+	// For example, the registered domain for " www.east.mydomain.co.uk " is
+	// "mydomain.co.uk".
 	// This value can be determined precisely with a list like the public
 	// suffix list (http://publicsuffix.org). Trying to approximate this by
 	// simply taking the last two labels will not work well for TLDs such as
@@ -60,27 +66,31 @@ type Destination struct {
 	RegisteredDomain string `ecs:"registered_domain"`
 
 	// The effective top level domain (eTLD), also known as the domain suffix,
-	// is the last part of the domain name. For example, the top level domain
-	// for google.com is "com".
+	// is the last part of the domain name and is typically inclusive of the
+	// top level domain (e.g. "co"), as well as including country code and
+	// region codes For example, the top level domain for
+	// www.east.mydomain.co.uk is "co.uk".
 	// This value can be determined precisely with a list like the public
-	// suffix list (http://publicsuffix.org). Trying to approximate this by
-	// simply taking the last label will not work well for effective TLDs such
-	// as "co.uk".
+	// suffix list (http://publicsuffix.org).
 	TopLevelDomain string `ecs:"top_level_domain"`
 
-	// The subdomain is all of the labels under the registered_domain.
+	// The subdomain portion of a fully qualified domain name includes all of
+	// the names except  the host name under the registered_domain.  In a
+	// partially qualified domain, or if the  the qualification level of the
+	// full name cannot be determined, subdomain contains all of the names
+	// below the registered domain.
+	// For example the subdomain portion of "www.east.mydomain.co.uk " is
+	// "east".
 	// If the domain has multiple levels of subdomain, such as
 	// "sub2.sub1.example.com", the subdomain field should contain "sub2.sub1",
 	// with no trailing period.
 	Subdomain string `ecs:"subdomain"`
 
-	// Hostname of the destination system.
-	// It normally contains what the `hostname` command returns on the host
-	// machine, or the host portion of a fully qualified domain name.
-	Hostname string `ecs:"hostname"`
-
-	// The country code portion of the domain.
-	CountryCode string `ecs:"country_code"`
+	// Internationalized domain names may contain non LDH-ASCII character
+	// encoding used to represent full unicode characters in the ASCII
+	// representation used by DNS. For example the  internationalized country
+	// code TLD for Israel is .xn--4dbrk0ce.
+	IsInternationalized bool `ecs:"is_internationalized"`
 
 	// Bytes sent from the destination to the source.
 	Bytes int64 `ecs:"bytes"`
--- code/go/ecs/dns.go
+++ code/go/ecs/dns.go
@@ -58,39 +58,48 @@ type Dns struct {
 	// respectively.
 	QuestionName string `ecs:"question.name"`
 
+	// The hostname is the host portion of a fully qualified domain name.
+	// For example, the hostname portion of "www.east.mydomain.co.uk " is
+	// "www". If the qualification level cannot be determined hostname need not
+	// be filled out.
+	QuestionHostname string `ecs:"question.hostname"`
+
 	// The highest registered domain, stripped of the subdomain.
-	// For example, the registered domain for "foo.google.com" is "google.com".
+	// For example, the registered domain for "www.east.mydomain.co.uk" is
+	// "mydomain.co.uk".
 	// This value can be determined precisely with a list like the public
 	// suffix list (http://publicsuffix.org). Trying to approximate this by
-	// simply taking the last two labels will not work well for TLDs such as
-	// "co.uk".
+	// simply taking the last two labels will not work well for TLDs which
+	// include country and/or region codes () such as "co.uk".
 	QuestionRegisteredDomain string `ecs:"question.registered_domain"`
 
 	// The effective top level domain (eTLD), also known as the domain suffix,
-	// is the last part of the domain name. For example, the top level domain
-	// for google.com is "com".
+	// is the last part of the domain name.
+	// For example, the top level domain for "www.east.mydomain.co.uk" is
+	// "mydomain.co.uk"
 	// This value can be determined precisely with a list like the public
 	// suffix list (http://publicsuffix.org). Trying to approximate this by
 	// simply taking the last label will not work well for effective TLDs such
 	// as "co.uk".
 	QuestionTopLevelDomain string `ecs:"question.top_level_domain"`
 
-	// The subdomain is all of the labels under the registered_domain in a
-	// partially qualified domain name or the qualification level of the full
-	// name cannot be determined, and all  of the labels except the hostname in
-	// a fully qualified domain name.
+	// The subdomain portion of a fully qualified domain name includes all of
+	// the names except  the host name under the registered_domain.  In a
+	// partially qualified domain, or if the  the qualification level of the
+	// full name cannot be determined, subdomain contains all of the names
+	// below the registered domain.
+	// For example the subdomain portion of "www.east.mydomain.co.uk " is
+	// "east".
 	// If the domain has multiple levels of subdomain, such as
 	// "sub2.sub1.example.com", the subdomain field should contain "sub2.sub1",
 	// with no trailing period.
 	QuestionSubdomain string `ecs:"question.subdomain"`
 
-	// The hostname is the host portion of a fully qualified domain name.
-	// If the qualification level cannot be determined hostname need not be
-	// filled out.
-	QuestionHostname string `ecs:"question.hostname"`
-
-	// The country code portion of the domain.
-	QuestionCountryCode string `ecs:"question.country_code"`
+	// Internationalized domain names may contain non LDH-ASCII character
+	// encoding used to represent full unicode characters in the ASCII
+	// representation used by DNS. For example the  internationalized country
+	// code TLD for Israel is .xn--4dbrk0ce.
+	IsInternationalized bool `ecs:"is_internationalized"`
 
 	// The type of record being queried.
 	QuestionType string `ecs:"question.type"`
