b69eab9b03cae2794b5635525f8723d30aed5c1d
Eric Beahan <eric.beahan@elastic.co>
2020-09-29 10:37:38 -0500

    undo changes to host field definitions

--- code/go/ecs/host.go
+++ code/go/ecs/host.go
@@ -64,32 +64,4 @@ type Host struct {
 	// or NetBIOS domain name. For Linux this could be the domain of the host's
 	// LDAP provider.
 	Domain string `ecs:"domain"`
-
-	// The highest registered domain, stripped of the subdomain. For example,
-	// the registered domain for " www.east.mydomain.co.uk " is
-	// "mydomain.co.uk". This value can be determined precisely with a list
-	// like the public suffix list (http://publicsuffix.org). Trying to
-	// approximate this by simply taking the last two labels will not work well
-	// for TLDs such as "co.uk".
-	RegisteredDomain string `ecs:"registered_domain"`
-
-	// The effective top level domain (eTLD), also known as the domain suffix,
-	// is the last part of the domain name and is typically inclusive of the
-	// top level domain (e.g. "co"), as well as including country code and
-	// region codes For example, the top level domain for
-	// www.east.mydomain.co.uk is "co.uk". This value can be determined
-	// precisely with a list like the public suffix list
-	// (http://publicsuffix.org).
-	TopLevelDomain string `ecs:"top_level_domain"`
-
-	// The subdomain portion of a fully qualified domain name includes all of
-	// the names except the host name under the registered_domain.  In a
-	// partially qualified domain, or if the the qualification level of the
-	// full name cannot be determined, subdomain contains all of the names
-	// below the registered domain.
-	// For example the subdomain portion of "www.east.mydomain.co.uk" is
-	// "east". If the domain has multiple levels of subdomain, such as
-	// "sub2.sub1.example.com", the subdomain field should contain "sub2.sub1",
-	// with no trailing period.
-	Subdomain string `ecs:"subdomain"`
 }
