0796cc3437dc50e47bca7677825edbe546881b3e
Mathieu Martin <mathieu.martin@elastic.co>
2020-10-02 15:11:52 -0400

    Try to clarify the meaning of the non-directional 'internal' and 'external' values.
    Also quoting the values, when they're mentioned in the description.

--- code/go/ecs/network.go
+++ code/go/ecs/network.go
@@ -70,10 +70,17 @@ type Network struct {
 	//   * unknown
 	//
 	// When mapping events from a host-based monitoring context, populate this
-	// field from the host's point of view, using the values ingress or egress.
+	// field from the host's point of view, using the values "ingress" or
+	// "egress".
 	// When mapping events from a network or perimeter-based monitoring
 	// context, populate this field from the point of view of your network
-	// perimeter, using the values inbound, outbound, internal or external.
+	// perimeter, using the values "inbound", "outbound", "internal" or
+	// "external".
+	// Note that "internal" is not crossing perimeter boundaries, and is meant
+	// to describe communication between two hosts within the perimeter. Note
+	// also that "external" is meant to describe traffic between two hosts that
+	// are external to their perimeter. This could for example be useful for
+	// ISPs or VPN service providers.
 	Direction string `ecs:"direction"`
 
 	// Host IP address when the source IP address is the proxy.
