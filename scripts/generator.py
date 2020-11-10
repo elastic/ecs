53d5aff5685bfac45077ef3a8be8e62001160946
Eric Beahan <eric.beahan@elastic.co>
2020-11-10 09:07:44 -0600

    Add --ref support for experimental artifacts (#1063)
    Co-authored-by: Mathieu Martin <webmat@gmail.com>

--- scripts/generator.py
+++ scripts/generator.py
@@ -63,7 +63,8 @@ def main():
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--ref', action='store', help='git reference to use when building schemas')
+    parser.add_argument('--ref', action='store', help='Loads fields definitions from `./schemas` subdirectory from specified git reference. \
+                                                       Note that "--include experimental/schemas" will also respect this git ref.')
     parser.add_argument('--include', nargs='+',
                         help='include user specified directory of custom field definitions')
     parser.add_argument('--subset', nargs='+',
