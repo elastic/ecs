28a3a69f4e67ca01812c02a4fbb3b0a9bb3209ff
Mathieu Martin <mathieu.martin@elastic.co>
2020-11-12 16:48:47 -0500

    [1.x] Indicate when artifacts include experimental changes (#1117) (#1125)

--- scripts/generator.py
+++ scripts/generator.py
@@ -41,6 +41,10 @@ def main():
     # statements like this after any step of interest.
     # ecs_helpers.yaml_dump('ecs.yml', fields)
 
+    # Detect usage of experimental changes to tweak artifact version label
+    if loader.EXPERIMENTAL_SCHEMA_DIR in args.include:
+        ecs_version += "+exp"
+
     fields = loader.load_schemas(ref=args.ref, included_files=args.include)
     if args.oss:
         oss.fallback(fields)
