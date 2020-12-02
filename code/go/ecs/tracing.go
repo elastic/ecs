154f082d47f8ffba469ab17136bd3f79ade36129
Mathieu Martin <mathieu.martin@elastic.co>
2020-12-02 15:29:34 -0500

    Add notice to the tracing field set, about not nesting field names. (#1162)

--- code/go/ecs/tracing.go
+++ code/go/ecs/tracing.go
@@ -23,6 +23,9 @@ package ecs
 // microservice architecture all in one view. This is accomplished by tracing
 // all of the requests - from the initial web request in the front-end service
 // - to queries made through multiple back-end services.
+// Unlike most field sets in ECS, the tracing fields are *not* nested under the
+// field set name. In other words, the correct field name is `trace.id`, not
+// `tracing.trace.id`, and so on.
 type Tracing struct {
 	// Unique identifier of the trace.
 	// A trace groups multiple events like transactions that belong together.
