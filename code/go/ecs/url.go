0843b28124187a68be0927a2c61ef1d1b66487af
Eric Beahan <eric.beahan@elastic.co>
2020-12-02 17:15:25 -0600

    update artifacts

--- code/go/ecs/url.go
+++ code/go/ecs/url.go
@@ -42,9 +42,9 @@ type Url struct {
 	// In some cases a URL may refer to an IP and/or port directly, without a
 	// domain name. In this case, the IP address would go to the `domain`
 	// field.
-	// If the URL contains a literal IPv6 address, `domain` may include the `[`
-	// and `]` characters enclosing the address
-	// (https://www.ietf.org/rfc/rfc2732.txt[RFC 2732]).
+	// If the URL contains a literal IPv6 address enclosed by `[` and `]` (IETF
+	// RFC 2732), the `[` and `]` characters should also be captured in the
+	// `domain` field.
 	Domain string `ecs:"domain"`
 
 	// The highest registered url domain, stripped of the subdomain.
