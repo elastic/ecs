603651944f5d2534b89906a9f1a5adcd506012f6
Eric Beahan <eric.beahan@elastic.co>
2020-12-04 14:42:25 -0600

    remove superfluous test

--- scripts/tests/test_es_template.py
+++ scripts/tests/test_es_template.py
@@ -222,33 +222,6 @@ class TestGeneratorsEsTemplate(unittest.TestCase):
         es_template.es6_type_fallback(test_map)
         self.assertEqual(test_map, exp)
 
-    def test_es6_fallback_recursive_case_constant_keyword(self):
-        test_map = {
-            "top_field": {
-                "properties": {
-                    "field": {
-                        "name": "field",
-                        "type": "constant_keyword"
-                    }
-                }
-            }
-        }
-
-        exp = {
-            "top_field": {
-                "properties": {
-                    "field": {
-                        "name": "field",
-                        "type": "keyword",
-                        "ignore_above": 1024
-                    }
-                }
-            }
-        }
-
-        es_template.es6_type_fallback(test_map)
-        self.assertEqual(test_map, exp)
-
 
 if __name__ == '__main__':
     unittest.main()
