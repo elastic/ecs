83ced31c9da6b3e5599b01e319a827a7941e6439
Mathieu Martin <mathieu.martin@elastic.co>
2020-12-10 16:19:49 -0500

    Move _meta section back inside mappings, in legacy templates. (#1186)
    This fixes an issue introduced by #1156, discovered in #1180. Composable templates support `_meta` at the template's root, but legacy templates don't. So we're just putting it back inside the mappings for legacy templates.
    
    This also fixes missing updates to the component template, after the introduction of wildcard in #1098.

--- scripts/generators/es_template.py
+++ scripts/generators/es_template.py
@@ -192,19 +192,24 @@ def template_settings(es_version, ecs_version, mappings_section, template_settin
             template = json.load(f)
     else:
         template = default_template_settings(ecs_version)
+
     if es_version == 6:
-        es6_mappings_section = copy.deepcopy(mappings_section)
-        es6_type_fallback(es6_mappings_section['properties'])
+        mappings_section = copy.deepcopy(mappings_section)
+        es6_type_fallback(mappings_section['properties'])
 
         # error.stack_trace needs special handling to set
         # index: false and doc_values: false
-        error_stack_trace_mappings = es6_mappings_section['properties']['error']['properties']['stack_trace']
+        error_stack_trace_mappings = mappings_section['properties']['error']['properties']['stack_trace']
         error_stack_trace_mappings.setdefault('index', False)
         error_stack_trace_mappings.setdefault('doc_values', False)
 
-        template['mappings'] = {'_doc': es6_mappings_section}
+        template['mappings'] = {'_doc': mappings_section}
     else:
         template['mappings'] = mappings_section
+
+    # _meta can't be at template root in legacy templates, so moving back to mappings section
+    mappings_section['_meta'] = template.pop('_meta')
+
     return template
 
 
