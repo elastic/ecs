5e84e9eaa74ccc6fd501ca17b1656c12d21c3516
Gil Raphaelli <gil@elastic.co>
2021-01-05 13:06:29 -0500

    add http.request.id (#1208)
    Co-authored-by: Eric Beahan <eric.beahan@elastic.co>
    # Conflicts:
    #	experimental/generated/csv/fields.csv
    #	generated/csv/fields.csv

--- code/go/ecs/http.go
+++ code/go/ecs/http.go
@@ -22,6 +22,12 @@ package ecs
 // Fields related to HTTP activity. Use the `url` field set to store the url of
 // the request.
 type Http struct {
+	// A unique identifier for each HTTP request to correlate logs between
+	// clients and servers in transactions.
+	// The id may be contained in a non-standard HTTP header, such as
+	// `X-Request-ID` or `X-Correlation-ID`.
+	RequestID string `ecs:"request.id"`
+
 	// HTTP request method.
 	// Prior to ECS 1.6.0 the following guidance was provided:
 	// "The field value must be normalized to lowercase for querying."
