3a35ee900f7be804b21ce874ca7eb22ea2283370
Eric Beahan <eric.beahan@elastic.co>
2021-02-11 11:36:44 -0600

    using array examples where array values are expected

--- code/go/ecs/host.go
+++ code/go/ecs/host.go
@@ -44,7 +44,7 @@ type Host struct {
 	// Host ip addresses.
 	IP string `ecs:"ip"`
 
-	// Host mac addresses.
+	// Host MAC addresses.
 	// The notation format from RFC 7042 is suggested: Each octet (that is,
 	// 8-bit byte) is represented by two [uppercase] hexadecimal digits giving
 	// the value of the octet as an unsigned integer. Successive octets are
