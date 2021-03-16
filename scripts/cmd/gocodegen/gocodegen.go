6654d028f42730d438fa985213069bf20c9e932d
Eric Beahan <eric.beahan@elastic.co>
2021-03-16 13:56:52 -0500

    flattened added to  case

--- scripts/cmd/gocodegen/gocodegen.go
+++ scripts/cmd/gocodegen/gocodegen.go
@@ -274,7 +274,7 @@ func goDataType(fieldName, elasticsearchDataType string) string {
 	}
 
 	switch elasticsearchDataType {
-	case "keyword", "wildcard", "version", "constant_keyword", "text", "ip", "geo_point":
+	case "keyword", "wildcard", "version", "constant_keyword", "text", "ip", "geo_point", "flattened":
 		return "string"
 	case "long":
 		return "int64"
