853b0c39500a50b0cd7a2e24226d1ce34780c102
djptek <dominic.page@elastic.co>
2021-05-13 19:48:13 +0200

    simplify console messages

--- scripts/schema/exclude_filter.py
+++ scripts/schema/exclude_filter.py
@@ -4,17 +4,18 @@ import os
 from schema import subset_filter
 
 # This script should be run downstream of the subset filters - it takes
-# all ECS and custom fields already loaded by the latter and explicitly 
+# all ECS and custom fields already loaded by the latter and explicitly
 # removes a subset, for example, to simulate impact of future removals
 
 
 def exclude(fields, exclude_file_globs, out_dir):
     excludes = load_exclude_definitions(exclude_file_globs)
-    
+
     if excludes:
         fields = exclude_fields(fields, excludes)
 
     return fields
+
 
 def pop_field(fields, path):
     '''pops a field from yaml derived dict using path derived from ordered list of nodes'''
@@ -58,5 +59,3 @@ def load_exclude_definitions(file_globs):
     if not excludes:
         raise ValueError('--exclude specified, but no exclusions found in {}'.format(file_globs))
     return excludes
-
-
