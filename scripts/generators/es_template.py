28704faf9af05ee6844af131ac987d9d4d0fa883
Eric Beahan <eric.beahan@elastic.co>
2021-07-21 09:45:07 -0500

    Support multi-field type fallback (#1528) (#1531)
    * support multi-field fallback
    
    * add match_only_text fallback
    
    * add tests for match_only_text multi-field support
    
    * comments for posterity
    
    * CHANGELOG.next entry

--- scripts/generators/es_template.py
+++ scripts/generators/es_template.py
@@ -277,5 +277,13 @@ def es6_type_fallback(mappings):
             if fallback_type:
                 mappings[name]['type'] = fallback_type
                 field_or_multi_field_datatype_defaults(mappings[name])
+        # support multi-fields
+        if 'fields' in details:
+            # potentially multiple multi-fields
+            for field_name, field_value in details['fields'].items():
+                fallback_type = TYPE_FALLBACKS.get(field_value['type'])
+                if fallback_type:
+                    mappings[name]['fields'][field_name]['type'] = fallback_type
+                    field_or_multi_field_datatype_defaults(mappings[name]['fields'][field_name])
         if 'properties' in details:
             es6_type_fallback(details['properties'])
