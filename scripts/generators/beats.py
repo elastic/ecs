7a4a2b6738ba430bb11f19e88cb46effba2bf29f
Adrian Serrano <adrisr83@gmail.com>
2021-10-21 15:06:38 +0200

    Allow for a fieldset's default_field to be set to false
    In case in the future we want to have a whole fieldset excluded from
    default_fields.

--- scripts/generators/beats.py
+++ scripts/generators/beats.py
@@ -23,7 +23,8 @@ def generate(ecs_nested, ecs_version, out_dir):
             continue
 
         beats_field = ecs_helpers.dict_copy_keys_ordered(fieldset, allowed_fieldset_keys)
-        beats_field['default_field'] = True
+        if 'default_field' not in beats_field:
+            beats_field['default_field'] = True
         beats_field['fields'] = fieldset_field_array(fieldset['fields'], df_allowlist, fieldset['prefix'])
         beats_fields.append(beats_field)
 
