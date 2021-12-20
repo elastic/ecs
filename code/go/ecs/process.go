06a69565664d640ed998fc2cbd0f9a576a137423
Kylie Geller <kylie.geller@elastic.co>
2021-12-20 11:29:55 -0500

    Ppid deprecation notice (#1699)

--- code/go/ecs/process.go
+++ code/go/ecs/process.go
@@ -45,6 +45,8 @@ type Process struct {
 	// Sometimes called program name or similar.
 	Name string `ecs:"name"`
 
+	// Deprecated for removal in the next major version. This field is
+	// superseded by `process.parent.pid`.
 	// Parent process' pid.
 	PPID int64 `ecs:"ppid"`
 
