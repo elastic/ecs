26606f55f5c1ceb51d8f8ca72991bc615e6019ea
Kylie Geller <kylie.geller@elastic.co>
2022-02-09 10:47:16 -0500

    better naming

--- scripts/tests/test_es_template.py
+++ scripts/tests/test_es_template.py
@@ -187,28 +187,28 @@ class TestGeneratorsEsTemplate(unittest.TestCase):
 
     def test_legacy_template_settings_override(self):
         ecs_version = 100
-        exp = es_template.default_legacy_template_settings(ecs_version)
+        default = es_template.default_legacy_template_settings(ecs_version)
 
         generated_template = es_template.template_settings(ecs_version, None, None, isLegacy=True)
-        self.assertEqual(generated_template, exp)
+        self.assertEqual(generated_template, default)
 
         generated_template = es_template.template_settings(
             ecs_version, None, './usage-example/fields/template-settings-legacy.json', isLegacy=True)
-        self.assertNotEqual(generated_template, exp)
+        self.assertNotEqual(generated_template, default)
 
     def test_default_composable_template_settings(self):
         ecs_version = 100
-        exp = es_template.default_template_settings(ecs_version)
+        default = es_template.default_template_settings(ecs_version)
         # Setting these to empty since we aren't testing this piece
-        exp['template']['mappings'] = None
-        exp['composed_of'] = None
+        default['template']['mappings'] = None
+        default['composed_of'] = None
 
         generated_template = es_template.template_settings(ecs_version, None, None)
-        self.assertEqual(generated_template, exp)
+        self.assertEqual(generated_template, default)
 
         generated_template = es_template.template_settings(
             ecs_version, None, './usage-example/fields/template-settings.json', isLegacy=True)
-        self.assertNotEqual(generated_template, exp)
+        self.assertNotEqual(generated_template, default)
 
 
 if __name__ == '__main__':
