d94f782407cab1ccb77736f91a61606b52a4394e
Eric Beahan <eric.beahan@elastic.co>
2022-02-11 10:36:29 -0600

    add type hints to subset_filter

--- scripts/schema/subset_filter.py
+++ scripts/schema/subset_filter.py
@@ -16,27 +16,42 @@
 # under the License.
 
 import os
+from typing import (
+    Any,
+    Dict,
+    List,
+)
+
 from generators import intermediate_files
-from schema import cleaner, loader
+from schema import (
+    cleaner,
+    loader,
+)
+from schema.types import (
+    FieldEntry
+)
 
 # This script takes all ECS and custom fields already loaded, and lets users
 # filter out the ones they don't need.
 
 
-def filter(fields, subset_file_globs, out_dir):
-    subsets = load_subset_definitions(subset_file_globs)
+def filter(
+    fields: Dict[str, FieldEntry],
+    subset_file_globs: List[str],
+    out_dir: str
+) -> Dict[str, FieldEntry]:
+    subsets: List[Dict[str, Any]] = load_subset_definitions(subset_file_globs)
     for subset in subsets:
-        subfields = extract_matching_fields(fields, subset['fields'])
+        subfields: Dict[str, FieldEntry] = extract_matching_fields(fields, subset['fields'])
         intermediate_files.generate(subfields, os.path.join(out_dir, 'ecs', 'subset', subset['name']), False)
 
-    merged_subset = combine_all_subsets(subsets)
+    merged_subset: Dict[str, Any] = combine_all_subsets(subsets)
     if merged_subset:
         fields = extract_matching_fields(fields, merged_subset)
-
     return fields
 
 
-def combine_all_subsets(subsets):
+def combine_all_subsets(subsets: Dict[str, Any]) -> Dict[str, Any]:
     """Merges N subsets into one. Strips top level 'name' and 'fields' keys as well as non-ECS field options since we can't know how to merge those."""
     merged_subset = {}
     for subset in subsets:
@@ -45,26 +60,26 @@ def combine_all_subsets(subsets):
     return merged_subset
 
 
-def load_subset_definitions(file_globs):
+def load_subset_definitions(file_globs: List[str]) -> List[Dict[str, Any]]:
     if not file_globs:
         return []
-    subsets = loader.load_definitions(file_globs)
+    subsets: List[Dict[str, Any]] = loader.load_definitions(file_globs)
     if not subsets:
         raise ValueError('--subset specified, but no subsets found in {}'.format(file_globs))
     return subsets
 
 
-ecs_options = ['fields', 'enabled', 'index']
+ecs_options: List[str] = ['fields', 'enabled', 'index']
 
 
-def strip_non_ecs_options(subset):
+def strip_non_ecs_options(subset: Dict[str, Any]) -> None:
     for key in subset:
         subset[key] = {x: subset[key][x] for x in subset[key] if x in ecs_options}
         if 'fields' in subset[key] and isinstance(subset[key]['fields'], dict):
             strip_non_ecs_options(subset[key]['fields'])
 
 
-def merge_subsets(a, b):
+def merge_subsets(a: Dict[str, Any], b: Dict[str, Any]) -> None:
     """Merges field subset definitions together. The b subset is merged into the a subset. Assumes that subsets have been stripped of non-ecs options."""
     for key in b:
         if key not in a:
@@ -85,9 +100,12 @@ def merge_subsets(a, b):
             a[key].pop('index', None)
 
 
-def extract_matching_fields(fields, subset_definitions):
+def extract_matching_fields(
+    fields: Dict[str, FieldEntry],
+    subset_definitions: Dict[str, Any]
+) -> Dict[str, FieldEntry]:
     """Removes fields that are not in the subset definition. Returns a copy without modifying the input fields dict."""
-    retained_fields = {x: fields[x].copy() for x in subset_definitions}
+    retained_fields: Dict[str, FieldEntry] = {x: fields[x].copy() for x in subset_definitions}
     for key, val in subset_definitions.items():
         retained_fields[key]['field_details'] = fields[key]['field_details'].copy()
         for option in val:
