32e74873f0ad067db9f5854adeca2cbc3d943a45
Eric Beahan <eric.beahan@elastic.co>
2022-05-16 15:14:39 -0500

    Introduce "docs_only" attribute for subsets (#1909)
    * add docs_only_fields
    
    * first pass at apply docs_only to subset filter config
    
    * linting
    
    * update field details
    
    * handle case with no docs_only fields declared
    
    * removed defining docs_only fields here
    
    * cleanup
    
    * add new unit tests
    
    * document in usage
    
    * revert changes to beats generator and default_fields list
    
    * extra import
    
    * changelog
    
    * moving entry
    
    (cherry picked from commit 9ca4cf329c60636b7fd44e1efd36af3a979659ce)
    
    # Conflicts:
    #	CHANGELOG.next.md

--- scripts/schema/subset_filter.py
+++ scripts/schema/subset_filter.py
@@ -15,11 +15,13 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import copy
 import os
 from typing import (
     Any,
     Dict,
     List,
+    Optional,
 )
 
 from generators import intermediate_files
@@ -48,6 +50,77 @@ def filter(
     merged_subset: Dict[str, Any] = combine_all_subsets(subsets)
     if merged_subset:
         fields = extract_matching_fields(fields, merged_subset)
+
+    # Looks for the `docs_only` attribute, which generates a second field subset
+    # to pass to the ascii_doc generator
+    # After second subset is generated, `docs_only: True` fields are removed
+    # from the `fields` subset
+    docs_only_field_paths = generate_docs_only_paths(merged_subset)
+    if docs_only_field_paths:
+        docs_only_subset = generate_docs_only_subset(docs_only_field_paths)
+        docs_only_fields = extract_matching_fields(fields, docs_only_subset)
+        fields = remove_docs_only_entries(docs_only_field_paths, fields)
+    else:
+        docs_only_fields = {}
+    return fields, docs_only_fields
+
+
+def generate_docs_only_subset(paths: List[str]) -> Dict[str, Any]:
+    """
+    Takes paths list of `docs_only` fields and generates a subset
+    """
+    docs_only_subset = {}
+    for path in paths:
+        # split and reverse
+        split_path = path.split('.')[::-1]
+        current_obj = docs_only_subset
+        while len(split_path) > 1:
+            temp_path = split_path.pop()
+            if not current_obj.get(temp_path):
+                current_obj[temp_path] = {'fields': {}}
+            current_obj = current_obj[temp_path]['fields']
+        current_obj[split_path[-1]] = {}
+    return docs_only_subset
+
+
+def generate_docs_only_paths(
+    subset: Dict[str, Any],
+    filtered: Optional[Dict[str, Any]] = {},
+    parent: Optional[str] = '',
+    path: Optional[str] = '',
+    paths: Optional[List[str]] = [],
+) -> List[str]:
+    """
+    Returns a list of field paths: ['process.same_as_process'] for subset fields
+    marked as `docs_only: True`
+    """
+    for current in subset:
+        if subset[current].get('docs_only'):
+            path += f'.{current}'
+            paths.append(path)
+        if 'fields' in subset[current] and isinstance(subset[current]['fields'], dict):
+            if not parent:
+                path_name = current
+            else:
+                path_name = f'{parent}.{current}'
+            generate_docs_only_paths(subset[current]['fields'],
+                                     filtered=filtered,
+                                     parent=current,
+                                     path=path_name,
+                                     paths=paths
+                                     )
+    return paths
+
+
+def remove_docs_only_entries(paths: List[str], fields: Dict[str, FieldEntry]) -> Dict[str, FieldEntry]:
+    """
+    Removed each path in paths list from the fields object
+    """
+    for path in paths:
+        split_path = path.split('.')
+        field_set = split_path[0]
+        field = split_path[1]
+        del(fields[field_set]['fields'][field])
     return fields
 
 
@@ -69,7 +142,7 @@ def load_subset_definitions(file_globs: List[str]) -> List[Dict[str, Any]]:
     return subsets
 
 
-ecs_options: List[str] = ['fields', 'enabled', 'index']
+ecs_options: List[str] = ['fields', 'enabled', 'index', 'docs_only']
 
 
 def strip_non_ecs_options(subset: Dict[str, Any]) -> None:
